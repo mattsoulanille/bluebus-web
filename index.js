f5126f8b0c24b99206679b708465bf9c4a6b6c84
Matthew Soulanille <matthew@soulanille.net>
2018-10-02 16:22:32 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+var port = 8000;
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var rp = require("request-promise");
+const favicon = require("express-favicon");
+var path = require("path");
+
+app.use(favicon(path.join(__dirname, 'favicon.png')));
+
+app.use(express.static(__dirname));
+
+
+
+app.get("/busdata.shtml", async function(req, res) {
+    var bus_data;
+    try {
+	bus_data = await rp("http://www.brynmawr.edu/transportation/bico.shtml");
+    }
+    catch (err) {
+	// maybe catch some stuff here?
+	throw(err);
+    }
+
+    res.send(bus_data);
+});
+
+
+
+http.listen(port, function(){
+    console.log('listening on *:'+port);
+});
