99367a8169765311f686a1830b3afe6ad00659fd
Matthew Soulanille <matthew@soulanille.net>
2019-01-26 18:28:58 -0500

    Displaying times, but off by a few days.

--- js/bus-times-table-parser.js
+++ js/bus-times-table-parser.js
@@ -87,76 +87,8 @@ class nextBusTime {
 		return times;
 	}
 
-	async getBusTimeInSeconds(){
-		//declare variables to figure out what time a bus is at
-		var timeDictionary = {};
-		var times = await this.bus.getTimes();
-		var BMCTimesInSeconds = [];
-		var HCTimesInSeconds = [];
-		var secondsInMinute = 60;
-		var secondsInHour = secondsInMinute*60;
-		var secondsInDay = secondsInHour*24;
-		var BMC_day = 0;
-		var BMC_AM = 0;
-		var HC_day = 0;
-		var HC_AM  = 0;
-		var BMCprev_mod = 0;
-		var HCprev_mod = 0;
-		var BMCseenSecondMonday = false;
-		var HCseenSecondMonday = false;
-		for (var row in times["BrynMawr"]){
-			//We know that all of the times are of form dd:dd
-			var BMCSplitTime = times["BrynMawr"][row].split(":");
-			//When hour mod 12 decreases, we know we need to switch AM to PM or vice versa
-			//Similarly, when we switch from PM to AM, we know we need to change the day
-			if((BMCSplitTime[0] % 12) < BMCprev_mod){
-				BMC_AM = BMC_AM + 1;
-				if(BMC_AM % 2 == 0){
-					BMC_day = BMC_day + 1;
-					if(BMC_day==7){
-						BMC_day = 0;
-						//For Monday morning listed under Sunday, need it at the beginning of the array
-						BMCseenSecondMonday = true;
-					}
-				}
-			}
-			BMCprev_mod = BMCSplitTime[0] % 12;
-			var BMCTimeInSeconds = BMC_day*secondsInDay + ((Number(BMCSplitTime[0])%12) + (BMC_AM % 2)*12)*secondsInHour + Number(BMCSplitTime[1])*secondsInMinute;
-			if(BMCseenSecondMonday){
-				//If it's Monday, we need it at the beginning of the array, not the end
-				BMCTimesInSeconds.unshift(BMCTimeInSeconds);
-			}
-			else{
-				BMCTimesInSeconds.push(BMCTimeInSeconds);
-			}
-		}
-		for (var row in times["Haverford"]){
-			//We know that all of the times are of form dd:dd
-			var HCSplitTime = times["Haverford"][row].split(":");
-			//When hour mod 12 decreases, we know we need to switch AM to PM or vice versa
-			//Similarly, when we switch from PM to AM, we know we need to change the day
-			if((HCSplitTime[0] % 12) < HCprev_mod){
-				HC_AM = HC_AM + 1;
-				if(HC_AM % 2 == 0){
-					HC_day = HC_day + 1;
-					if(HC_day == 7){
-						HC_day = 0;
-						HCseenSecondMonday = true;
-					}
-				}
-			}
-			HCprev_mod = HCSplitTime[0] % 12;
-			var HCTimeInSeconds = HC_day*secondsInDay + ((Number(HCSplitTime[0])%12) + (HC_AM % 2)*12)*secondsInHour + Number(HCSplitTime[1])*secondsInMinute;
-			if(HCseenSecondMonday){
-				HCTimesInSeconds.unshift(HCTimeInSeconds);
-			}
-			else{
-				HCTimesInSeconds.push(HCTimeInSeconds);
-			}
-		}
-		timeDictionary["Haverford"] = HCTimesInSeconds;
-		timeDictionary["BrynMawr"] = BMCTimesInSeconds;
-		this.busTimesInSeconds = timeDictionary;
+	async getBusTimeInSeconds() {
+	    var res = await fetch("busdata.json");
+	    this.busTimesInSeconds = await res.json();
 	}
-
 }
