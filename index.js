63a24421d17b8c386445f7910167e446d4786e28
Matthew Soulanille <matthew@soulanille.net>
2019-02-14 20:33:08 -0500

    Move all static assets to ./static/

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 var port = 80;
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
+
+
+
+var server;
+if (process.env.NODE_ENV == "Production") {
+    // Use HTTPS
+    var privateKey = fs.readFileSync("keys/privatekey.pem");
+    var certificate = fs.readFileSync("keys/certificate.pem");
+    server = require('https').createServer({
+        key: privateKey,
+        cert: certificate
+    });
+}
+else {
+    // Use HTTP
+    server = require('http').Server(app);
+}
+
+
 var rp = require("request-promise");
 const favicon = require("express-favicon");
 var path = require("path");
@@ -9,7 +27,14 @@ const getBusData = require("./server/getBusData.js");
 
 app.use(favicon(path.join(__dirname, 'favicon.ico')));
 
-app.use(express.static(__dirname));
+
+
+app.use(express.static(path.join(__dirname, "static")));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+
 
 // Temporary
 getBusData("http://www.brynmawr.edu/transportation/bico.shtml");
@@ -17,11 +42,11 @@ getBusData("http://www.brynmawr.edu/transportation/bico.shtml");
 app.get("/busdata.json", async function(req, res) {
     var data = await getBusData("http://www.brynmawr.edu/transportation/bico.shtml");
     res.send(data);
-    
+
 });
 
 
 
-http.listen(port, function() {
-    console.log('listening on *:'+port);
+server.listen(port, function() {
+    console.log('listening on *:' + port);
 });
